4a6a91dd4bc081a3c1a2d47075027b3243bc4dbe
nagachi <rafly.rigannagachi@gmail.com>
2021-12-02 15:25:08 +0700

    edit input

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ max_date = min_date + datetime.timedelta(days=365)
 date = st.sidebar.date_input('Pick date: ', min_value=min_date, max_value=max_date)
 datediff = (date - min_date).days
 date_end = 356+datediff
-# st.write((date_end))
+st.write(("{} days after {}: \n".format(date_end, min_date)))
 
 # load the model from disk
 filename = 'sarimax_model.pkl'
@@ -20,5 +20,4 @@ loaded_model = pickle.load(open(filename, 'rb'))
 data_merge = pd.read_csv('data_merge.csv', parse_dates=[0], index_col=0)
 
 data_merge['forecast'] = loaded_model.predict(start = 356, end = date_end, dynamic= True)
-st.write(data_merge.head())
 st.line_chart(data_merge[['Total Pasien Isolasi', 'forecast']])
